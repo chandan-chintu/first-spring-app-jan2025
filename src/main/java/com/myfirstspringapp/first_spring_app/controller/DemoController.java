6cb79252657d6b3259dca2824f4d229993096637
Chandan K M <chandanchintu16@gmail.com>
2025-01-24 21:27:00 +0530

    Sample APIs writing

--- src/main/java/com/myfirstspringapp/first_spring_app/controller/DemoController.java
+++ src/main/java/com/myfirstspringapp/first_spring_app/controller/DemoController.java
@@ -1,9 +1,15 @@
 package com.myfirstspringapp.first_spring_app.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/demo/api")
 public class DemoController {
+
+    @GetMapping
+    public String getDemo(){
+        return "This is our first Demo API";
+    }
 }
--- src/main/java/com/myfirstspringapp/first_spring_app/controller/SampleController.java
+++ src/main/java/com/myfirstspringapp/first_spring_app/controller/SampleController.java
@@ -1,6 +1,7 @@
 package com.myfirstspringapp.first_spring_app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,4 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/sample/api")
 public class SampleController {
+
+    @GetMapping("/getsample")
+    public String getSample(){
+        return "This is our first sample API";
+    }
+
+    @GetMapping("/getsample2")
+    public String getSample2(){
+        return "This is our second sample API";
+    }
 }
